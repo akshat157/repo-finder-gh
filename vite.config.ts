93cd6a2fc3d51bdb2aafc1f2ee0d9d01dfb55449
Akshat Malviya <ancientfuture1337@gmail.com>
2026-03-16 07:44:20 +0900

    Update vite config to support deployment redirection on vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/projects/repo-finder-gh/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
