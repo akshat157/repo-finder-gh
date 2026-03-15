06f9032b3bcc46382515c02d57cb9adbe015d3f1
Akshat Malviya <ancientfuture1337@gmail.com>
2026-03-16 02:01:46 +0900

    Add more fetchable properties to API.

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,30 +1,53 @@
 import type { TRepo } from "@/types/TRepo"
-import type { TSortReposBy } from "@/types/TSortBy"
+import type { TSearchResult } from "@/types/TSearchResult"
+import type { TSortReposBy } from "@/types/TSortReposBy"
 import { Octokit } from "@octokit/rest"
 
-export async function SearchRepositories({
+const octokit = new Octokit()
+
+export async function SearchRepos({
   q,
   sortBy,
+  page = 1,
+  perPage = 10,
 }: {
   q: string
   sortBy?: TSortReposBy
-}): Promise<TRepo[] | null> {
-  const octokit = new Octokit()
+  page: number
+  perPage: number
+}): Promise<TSearchResult | null> {
+  try {
+    const result = await octokit.rest.search.repos({
+      q,
+      sort: sortBy,
+      page,
+      per_page: perPage,
+    })
+    if (!result) {
+      return null
+    }
+    const repos = result.data.items.map(
+      (item) =>
+        ({
+          id: item.id,
+          name: item.name,
+          owner: item.owner?.login || null,
+          stars: item.stargazers_count,
+          url: item.html_url,
+          description: item.description || null,
+          topics: item.topics,
+          language: item.language,
+        }) as TRepo
+    )
 
-  const result = await octokit.rest.search.repos({
-    q,
-    sort: sortBy,
-  })
+    const totalCount = result.data.total_count
 
-  if (!result) {
+    return {
+      repos,
+      totalCount,
+    }
+  } catch (error) {
+    console.error(error)
     return null
   }
-  const repos = result.data.items.map((item) => ({
-    name: item.name,
-    owner: item.owner?.login || null,
-    stars: item.stargazers_count,
-    url: item.html_url,
-  }))
-
-  return repos
 }
